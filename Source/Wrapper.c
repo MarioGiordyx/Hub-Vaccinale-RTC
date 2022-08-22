7ea382a500d5a6c8ddde089021ac7c4c24348082
Simone Cioffi <56473031+SimoneCff@users.noreply.github.com>
2022-08-22 14:45:05 +0200

    Added FullWrite and FullRead to Wrapper

--- Source/Wrapper.c
+++ Source/Wrapper.c
@@ -38,3 +38,42 @@ int wrapped_accept(int SockDescriptor, struct sockaddr * addr, socklen_t addrlen
     }
     return CommmSockDescriptor;
 }
+
+ssize_t wrapped_fullwrite(int fd, const void * buf, size_t count){
+    size_t nleft;
+    ssize_t nwritten;
+    nleft = count;
+    while (nleft > 0) {
+        if ((nwritten == write(fd,buf,nleft)) < 0) {
+            if (errno = EINTR) {
+                continue;
+            } else {
+                exit(nwritten);
+            }
+        }
+        nleft -=nwritten;
+        buf += nwritten;
+    }
+    return (nleft);
+}
+
+ssize_t wrapped_fullread(int fd, const void * buf, size_t count){
+    size_t nleft;
+    ssize_t nread;
+    nleft = count;
+    while (nleft > 0){
+        if ((nread = read(fd,buf,nleft)) < 0) {
+            if (errno == EINTR) {
+                continue;
+            } else {
+                exit(nread);
+            }
+        } else if (nread == 0) {
+            break;
+        }
+        nleft -=nread;
+        buf +=nread;
+    }
+    buf = 0;
+    return (nleft);
+}
--- Source/Wrapper.h
+++ Source/Wrapper.h
@@ -6,12 +6,16 @@
 #include <sys/socket.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <errno.h>
 
 int wrapped_socket(int family, int type, int protocol);
 void wrapped_connect(int SockDescriptor, struct sockaddr * addr, socklen_t addrlen);
 void wrapped_bind(int SockDescriptor, struct sockaddr * addr, socklen_t addrlen);
 void wrapped_listen(int SockDescriptor, int connNum);
 int wrapped_accept(int SockDescriptor, struct sockaddr * addr, socklen_t addrlen);
+
+ssize_t wrapped_fullwrite(int fd, const void * buf, size_t count);
+ssize_t wrapped_fullread(int fd, const void * buf, size_t count);
 
 
 #endif 
