0323a688e4e49ee83b325fe94a869b07ad243b34
Simone Cioffi <56473031+SimoneCff@users.noreply.github.com>
2022-08-23 15:26:30 +0200

    Finisched ClientC

--- Source/Client.c
+++ Source/Client.c
@@ -10,17 +10,16 @@
 
 int main(int argc, char **argv) {
 	int CVsk,n;
-	char* sendB;
-	char recB[21];
+	char* BufferCF;
 	struct sockaddr_in servaddr;
 
 	//Controllo argomenti
 	if (argc != 3) {
-		fprintf(stderr,"Usage: %s <IP-Address> %s <CF>",argv[1],argv[2]);
+		fprintf(stderr,"Attenzione, il formato di input deve essere : %s <IP-Address>,: %s <Ultime 8 cifre Tessera Sanitaria> \n",argv[1],argv[2]);
 		exit(1);
-	} else printf("IP %s, CF %s \n",argv[1],argv[2]);
+	}
 
-    //Setting Socket ASL;
+    //Setting Socket Centro Vacinnale;
 	CVsk = wrapped_socket(AF_INET,SOCK_STREAM,0);
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_port = htons(28);
@@ -31,21 +30,18 @@ int main(int argc, char **argv) {
 	}
 
 	//Check Connesione
+	printf("[+] Connesione Al centro vaccinale IP : %s \n", argv[1]);
+
 	wrapped_connect(CVsk, (struct sockaddr *)&servaddr, sizeof(servaddr));
 
-	printf("Inizio Mandando CFF a ASL \n");
-
-	//Write CF
-	sendB=argv[2];
-	sendB[21]='\0';
-
-	wrapped_fullwrite(CVsk,sendB,strlen(sendB));
-
-	printf("Mess Mandato, Aspetto Rispsota \n");
+	printf("Connesione Effetuata al Centro Vaccinale \n");
+	BufferCF= argv[2];
 	
-	//Read Risp
-	wrapped_fullread(CVsk,recB,sizeof(recB));
-	printf("<Mess Ricevuto da ASL> : %s \n",recB);
+	wrapped_fullwrite(CVsk,BufferCF,strlen(BufferCF));
+
+	printf("Ultime 8 cifre  mandate al Centro Vaccinale, Arrivederci !\n");
+	
+	close(CVsk);
 
     exit(0);
 
