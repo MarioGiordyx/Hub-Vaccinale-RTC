efd232d27a1156cb849df33e02ec52f1e53d8019
Simone Cioffi <56473031+SimoneCff@users.noreply.github.com>
2022-08-23 16:09:32 +0200

    Boh

--- Source/CentroVaccinale.c
+++ Source/CentroVaccinale.c
@@ -5,21 +5,27 @@
 #include <stdlib.h>
 #include <string.h>
 #include "Wrapper.h"
+#include "util.h"
 
 int main(int argc, char *argv[]){
     int list_fd, conn_fd;
-    struct sockaddr_in serv_add;
+    struct sockaddr_in server_cv , server_v;
     socklen_t len;
-    char buffer[21];
+    char buffer[8];
     pid_t pid;
     
     list_fd=wrapped_socket(AF_INET,SOCK_STREAM,0);
 
-    serv_add.sin_family = AF_INET;
-    serv_add.sin_addr.s_addr = htonl(INADDR_ANY);
-    serv_add.sin_port = htons(28);
+    //SockAddr del CentroVacicnale
+    server_cv.sin_family = AF_INET;
+    server_cv.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_cv.sin_port = htons(28);
 
-    wrapped_bind(list_fd,(struct sockaddr *)&serv_add,sizeof(serv_add));
+    //SockAddr del ServerV
+    server_v.sin_family = AF_INET;
+    server_v.sin_port= htons(31);
+
+    wrapped_bind(list_fd,(struct sockaddr *)&server_cv,sizeof(server_cv));
 
     printf("[+] Bind effettuato \n");
     wrapped_listen(list_fd,1024);
@@ -32,20 +38,37 @@ int main(int argc, char *argv[]){
 
         if(pid<0){
             perror ("Fork errato");
+            close(conn_fd);
             exit(-1);
         }
+
+        // Child
         if (pid==0){
-            printf("[+] Connesione effettuata da un Client ! \n");
+            close(conn_fd);
+            
+
+            //Lettura 8 cifre Tessesa Sanitaria
+            printf("[+] Lettura in corso delle 8 cifre mandate da un client \n");
             
             wrapped_fullread(conn_fd,buffer,sizeof(buffer));
             
-            printf("[+] Ricevuto Da Client: %s \n",buffer);
+            printf("[+] Lettura Effetuata, ");
 
-            char *message = "Ricevuto-Conv";
-            wrapped_fullwrite(conn_fd,message,strlen(message));
+            //Creazione Socket & connesione con ServerV
+
+            int ServerV_sock = wrapped_socket(AF_INET,SOCK_STREAM,0);
+
+            wrapped_connect(ServerV_sock,(struct sockaddr_in *) &server_v, sizeof(server_v));
+            printf("[+] Connessione Effettuata al ServerV, creazione Package \n");
+
+            struct record_gp * record = create_record(buffer,6,0);
+
+            printf("[+] Record Creato, invio in corso al ServerV");
+
+            wrapped_fullwrite(ServerV_sock,record,sizeof(record));
+
+            printf("[+] Record inviato, Chiusura Fork");
             
-            printf("[-] Messaggio Mandato, Terminazione Fork \n");
-            close(conn_fd);
             exit(0);
         } else {
             close(0);
