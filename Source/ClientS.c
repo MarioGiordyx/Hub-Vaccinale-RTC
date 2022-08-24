efa10f5d59542aaf17ea430af8d8d8d7300f8fea
Mario Giordano <mario.giordano002@studenti.uniparthenope.it>
2022-08-24 14:38:02 +0000

    Added ClientS

---
+++ Source/ClientS.c
@@ -0,0 +1,57 @@
+#include "utility/Wrapper.h"
+#include "utility/utils.h"
+
+int main(int argc, char **argv) {
+	int ServerGsk,n;
+	char* BufferCF;
+	char validation[2];
+	struct sockaddr_in servaddr;
+
+	//Controllo argomenti
+	if (argc != 3) {
+		fprintf(stderr,"Attenzione, il formato di input deve essere : %s <IP-Address>,: %s <Ultime 8 cifre Tessera Sanitaria> \n",argv[1],argv[2]);
+		exit(1);
+	}
+
+    //Setting Socket Centro Vacinnale;
+	ServerGsk = wrapped_socket(AF_INET,SOCK_STREAM,0);
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(29);
+
+	if(inet_pton(AF_INET,argv[1],&servaddr.sin_addr)<= 0) {
+		fprintf(stderr,"inet_pton error \n");
+		exit(1);
+	}
+
+	//Check se l' input della TS è di 8 cifre
+	if (strlen(argv[2]) <= 0 || strlen(argv[2]) > 8){
+		fprintf(stderr,"Errore Input Tessera Sanitaria, size troppo piccolo \n");
+		exit(1);
+	}
+
+	BufferCF= argv[2];
+
+	//Check HASH per vedere se sono numeri compresi da 0 a 9
+	checkHash(BufferCF,sizeof(BufferCF));
+
+	//Check Connesione
+	printf("[+] Connesione Al serverG IP : %s \n", argv[1]);
+
+	wrapped_connect(ServerGsk, (struct sockaddr *)&servaddr, sizeof(servaddr));
+
+	printf("Connesione Effetuata al serverG \n");
+
+	wrapped_fullwrite(ServerGsk,BufferCF,strlen(BufferCF));
+
+	printf("Ultime 8 cifre  mandate al serverG, in attesa di convalida Green Pass...\n");
+
+	wrapped_fullread(ServerGsk, validation, sizeof(validation));
+	printf("Validità Green Pass: %s \n", validation);
+
+	printf("Arrivederci! \n");
+	
+	close(ServerGsk);
+
+    exit(0);
+
+}
